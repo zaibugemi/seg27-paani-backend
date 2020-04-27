087546fde5308b2b59054bb2dd1fba0831cbb5a1
Abbas <21100166@lums.edu.pk>
2020-04-27 17:37:40 +0500

    added fetch companies fn

--- server.js
+++ server.js
@@ -10,11 +10,13 @@ app.use(bodyparser.urlencoded({ extended: true })) //
 // setting up routes
 const userRoutes = require('./routes/user.routes'); // routes related to users are defined in routes/user.routes.js.
 const customerRoutes = require('./routes/customer_routes');
+const companyRoutes = require('./routes/company.routes');
 const orderRoutes = require('./routes/order.routes');
 //// IMPORT YOUR OWN ROUTES HERE !!
 
 app.use('/users', userRoutes); // tell express to forward requests at www.[whatever].com/users or any subdirectory of /users to userRoutes.js
 app.use('/customers', customerRoutes);
+app.use('/companies', companyRoutes);
 app.use('/orders', orderRoutes);
 
 port = process.env.PORT || 7777
