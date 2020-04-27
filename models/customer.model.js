dafb50d4bb9c6b3a0d833f6e15e3f8890b6cca5b
Zuhaib Shah <34958019+zaibugemi@users.noreply.github.com>
2020-04-27 11:34:49 -0400

    Update customer.model.js

--- models/customer.model.js
+++ models/customer.model.js
@@ -5,6 +5,12 @@ model = {
     create: async (customer) => {// query to add a new customer to the database
         return await query(`INSERT INTO paani.customer (name, contact_num, address, location ) VALUES (?,?,?,?)`, [customer.name, customer.contact_num, customer.address, customer.location ]);
     }, 
+	
+	getAll: async () => {
+		return await query(
+			`SELECT * FROM paani.customer`
+		);
+	},
 }
 
 module.exports = model
